74786c1122276e443bfcf4707e4af759bfe54ef0
Ecuacion <alajandro5gt01@gmail.com>
2015-06-17 15:32:59 +0200

    Remove example teams
    A bot template should not have any static team

--- features/battle/teams.js
+++ features/battle/teams.js
@@ -3,16 +3,5 @@
 */
 
 exports.teams = {
-	"ou": [
-		'Bisharp||leftovers||ironhead,knockoff,suckerpunch,swordsdance|Adamant|232,252,,,4,20|||||]Chansey||eviolite||healbell,seismictoss,softboiled,thunderwave|Bold|248,,252,,8,|||||]Venusaur-Mega||venusaurite||gigadrain,hiddenpowerfire,leechseed,synthesis|Bold|240,,224,44,,||,30,,30,,30|||]Heatran||airballoon||stealthrock,fireblast,toxic,taunt|undefined|224,,,16,48,220|||||]Skarmory||rockyhelmet|1|roost,defog,spikes,bravebird|Impish|248,,232,,,28|||||]Keldeo||leftovers||scald,secretsword,substitute,calmmind|Timid|,,,252,4,252|||||',
-		'Lopunny||lopunnite|H|return,highjumpkick,icepunch,fakeout|Jolly|,252,4,,,252|||||]Gengar||lifeorb||shadowball,sludgewave,focusblast,taunt|Timid|4,,,252,,252|||||]Garchomp||focussash|H|stealthrock,earthquake,outrage,fireblast|Naive|,252,,4,,252|||||]Starmie||leftovers|1|scald,reflecttype,recover,rapidspin|Timid|232,,24,4,,248||,0,,,,|||]Bisharp||blackglasses||knockoff,ironhead,suckerpunch,swordsdance|Adamant|4,252,,,,252|||||]Thundurus||leftovers||focusblast,thunderbolt,hiddenpowerice,thunderwave|Timid|,,4,252,,252||,30,30,,,|||',
-		'Charizard||charizarditex||dragondance,flareblitz,roost,dragonclaw|Jolly|96,220,,,8,184|M||||]Breloom||focussash|H|spore,rocktomb,bulletseed,machpunch|Jolly|4,252,,,,252|||||]Rotom-Wash||leftovers||voltswitch,hydropump,willowisp,painsplit|Bold|252,,212,,,44|||||]Landorus-Therian||choicescarf||knockoff,earthquake,stoneedge,uturn|Jolly|,252,4,,,252|||||]Jirachi||leftovers||ironhead,wish,stealthrock,thunderwave|Jolly|252,,,,4,252|||||]Latios||lifeorb||dracometeor,earthquake,hiddenpowerfire,defog|Naive|,,16,240,,252||,30,,30,,30|||',
-		'Dragonite||choiceband|H|outrage,extremespeed,firepunch,earthquake|Adamant|40,252,,,,216|||||]Charizard-Mega-Y||charizarditey||fireblast,roost,solarbeam,earthquake|Mild|,4,,252,,252|||||]Tyranitar||choicescarf||stoneedge,pursuit,crunch,earthquake|Jolly|,252,4,,,252|||||]Keldeo-Resolute||choicespecs||hydropump,scald,secretsword,hiddenpowerelectric|Timid|,,,252,4,252||,,,30,,|||]Landorus-Therian||leftovers||earthquake,stealthrock,stoneedge,uturn|Impish|240,,232,,16,20|||||]Mew||leftovers||defog,softboiled,knockoff,willowisp|Careful|248,,,,204,56|||||'
-	],
-	"gen5ou": [
-		'Mamoswine||focussash|H|earthquake,iceshard,endeavor,stealthrock|Adamant|4,252,,,,252|M||||]Latios||choicespecs||dracometeor,psychic,surf,hiddenpowerfire|Modest|56,,,252,4,196||,30,,30,,30|||]Scizor||choiceband|1|uturn,bulletpunch,superpower,quickattack|Adamant|240,252,,,,16|F||||]Kyurem|kyuremblack|choiceband||outrage,dragonclaw,fusionbolt,icebeam|Adamant|4,252,,,,252|||||]Salamence||choicescarf|H|outrage,dragonclaw,earthquake,fireblast|Jolly|,252,4,,,252|M||||]Magnezone||choicespecs||thunderbolt,hiddenpowerfire,flashcannon,voltswitch|Modest|4,,,252,,252||,30,,30,,30|||'
-	],
-	"gen4ou": [
-		'Rotom-Wash||leftovers||willowisp,thunderbolt,painsplit,shadowball|Modest|252,,,252,,|||||]Skarmory||leftovers|1|bravebird,spikes,whirlwind,roost|Impish|252,,232,,,24|||||]Tyranitar||choicescarf||stoneedge,pursuit,earthquake,crunch|Adamant|,252,,,4,252|||||]Blissey||leftovers||healbell,wish,seismictoss,counter|Bold|252,,252,,4,|||||]Forretress||leftovers||stealthrock,rapidspin,toxicspikes,bugbite|Impish|252,4,252,,,|||||]Roserade||blacksludge||gigadrain,sludgebomb,rest,aromatherapy|Modest|252,,,252,4,|||||'
-	]
+	//Example: "tier": ['team1', 'team2']
 };
