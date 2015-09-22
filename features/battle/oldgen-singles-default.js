982b55ad0fdc844e357073f4845ce459f263d67e
Ecuacion <alajandro5gt01@gmail.com>
2015-09-22 20:36:42 +0200

    Battle: Use type chart from Tools global
    This improves memory usage

--- features/battle/oldgen-singles-default.js
+++ features/battle/oldgen-singles-default.js
@@ -685,12 +685,10 @@ module.exports = {
 		},
 		"Electric": {
 			damageTaken: {
-				par: 3,
 				"Bug": 0,
 				"Dark": 0,
 				"Dragon": 0,
 				"Electric": 2,
-				"Fairy": 0,
 				"Fighting": 0,
 				"Fire": 0,
 				"Flying": 2,
@@ -801,12 +799,10 @@ module.exports = {
 		},
 		"Ghost": {
 			damageTaken: {
-				trapped: 3,
 				"Bug": 2,
 				"Dark": 1,
 				"Dragon": 0,
 				"Electric": 0,
-				"Fairy": 0,
 				"Fighting": 3,
 				"Fire": 0,
 				"Flying": 0,
@@ -825,12 +821,10 @@ module.exports = {
 		},
 		"Grass": {
 			damageTaken: {
-				powder: 3,
 				"Bug": 1,
 				"Dark": 0,
 				"Dragon": 0,
 				"Electric": 2,
-				"Fairy": 0,
 				"Fighting": 0,
 				"Fire": 1,
 				"Flying": 1,
@@ -996,14 +990,13 @@ module.exports = {
 				tox: 3,
 				sandstorm: 3,
 				"Bug": 2,
-				"Dark": 0,
+				"Dark": 2,
 				"Dragon": 2,
 				"Electric": 0,
-				"Fairy": 2,
 				"Fighting": 1,
 				"Fire": 1,
 				"Flying": 2,
-				"Ghost": 0,
+				"Ghost": 2,
 				"Grass": 2,
 				"Ground": 1,
 				"Ice": 2,
@@ -1039,106 +1032,5 @@ module.exports = {
 			},
 			HPivs: {"atk":30, "def":30, "spa":30}
 		}
-	},
-	TypeChartGen5: {
-		"Electric": {
-			inherit: true,
-			damageTaken: {
-				"Bug": 0,
-				"Dark": 0,
-				"Dragon": 0,
-				"Electric": 2,
-				"Fighting": 0,
-				"Fire": 0,
-				"Flying": 2,
-				"Ghost": 0,
-				"Grass": 0,
-				"Ground": 1,
-				"Ice": 0,
-				"Normal": 0,
-				"Poison": 0,
-				"Psychic": 0,
-				"Rock": 0,
-				"Steel": 2,
-				"Water": 0
-			}
-		},
-		"Ghost": {
-			inherit: true,
-			damageTaken: {
-				"Bug": 2,
-				"Dark": 1,
-				"Dragon": 0,
-				"Electric": 0,
-				"Fighting": 3,
-				"Fire": 0,
-				"Flying": 0,
-				"Ghost": 1,
-				"Grass": 0,
-				"Ground": 0,
-				"Ice": 0,
-				"Normal": 3,
-				"Poison": 2,
-				"Psychic": 0,
-				"Rock": 0,
-				"Steel": 0,
-				"Water": 0
-			}
-		},
-		"Grass": {
-			inherit: true,
-			damageTaken: {
-				"Bug": 1,
-				"Dark": 0,
-				"Dragon": 0,
-				"Electric": 2,
-				"Fighting": 0,
-				"Fire": 1,
-				"Flying": 1,
-				"Ghost": 0,
-				"Grass": 2,
-				"Ground": 2,
-				"Ice": 1,
-				"Normal": 0,
-				"Poison": 1,
-				"Psychic": 0,
-				"Rock": 0,
-				"Steel": 0,
-				"Water": 2
-			}
-		},
-		"Steel": {
-			inherit: true,
-			damageTaken: {
-				psn: 3,
-				tox: 3,
-				sandstorm: 3,
-				"Bug": 2,
-				"Dark": 2,
-				"Dragon": 2,
-				"Electric": 0,
-				"Fighting": 1,
-				"Fire": 1,
-				"Flying": 2,
-				"Ghost": 2,
-				"Grass": 2,
-				"Ground": 1,
-				"Ice": 2,
-				"Normal": 2,
-				"Poison": 3,
-				"Psychic": 2,
-				"Rock": 2,
-				"Steel": 2,
-				"Water": 0
-			}
-		},
-		"Fairy": null
 	}
 };
-
-for (var i in exports.TypeChartGen5) {
-	if (!exports.TypeChartGen5[i]) continue;
-	for (var j in exports.TypeChartGen5[i].damageTaken) {
-		exports.TypeChartGen6[i].damageTaken[j] = exports.TypeChartGen5[i].damageTaken[j];
-	}
-}
