0c1b9c01ff2a9d6f5c0e528cf7865a166ebf590a
Ecuacion <alajandro5gt01@gmail.com>
2015-10-08 18:00:51 +0200

    Battle: Avoid command spam on tour parsing
    Example: If a team is not valid

--- features/battle/tournaments.js
+++ features/battle/tournaments.js
@@ -1,6 +1,18 @@
+/*
+	Tournaments parser
+*/
 
+const ACTION_INTERVAL = 1500;
 
 var tourData = exports.tourData = {};
+var lastAction = exports.lastAction = {};
+
+var canSendCommands = exports.canSendCommands = function (room) {
+	var res = true;
+	if (lastAction[room] && Date.now() - lastAction[room] < ACTION_INTERVAL) res = false;
+	lastAction[room] = Date.now();
+	return res;
+};
 
 exports.clearData = function () {
 	for (var i in tourData)
@@ -27,15 +39,18 @@ exports.parse = function (room, message, isIntro, spl) {
 					if (Features['battle'].TeamBuilder.hasTeam(tourData[room].format)) Bot.say(room, '/tour join');
 				}
 			}
-
 			if (tourData[room].challenges && tourData[room].challenges.length) {
-				var team = Features['battle'].TeamBuilder.getTeam(tourData[room].format);
-				if (team) Bot.say(room, '/useteam ' + team);
-				for (var i = 0; i < tourData[room].challenges.length; i++) Bot.say(room, '/tour challenge ' + tourData[room].challenges[i]);
+				if (canSendCommands(room)) {
+					var team = Features['battle'].TeamBuilder.getTeam(tourData[room].format);
+					if (team) Bot.say(room, '/useteam ' + team);
+					for (var i = 0; i < tourData[room].challenges.length; i++) Bot.say(room, '/tour challenge ' + tourData[room].challenges[i]);
+				}
 			} else if (tourData[room].challenged) {
-				var team = Features['battle'].TeamBuilder.getTeam(tourData[room].format);
-				if (team) Bot.say(room, '/useteam ' + team);
-				Bot.say(room, '/tour acceptchallenge');
+				if (canSendCommands(room)) {
+					var team = Features['battle'].TeamBuilder.getTeam(tourData[room].format);
+					if (team) Bot.say(room, '/useteam ' + team);
+					Bot.say(room, '/tour acceptchallenge');
+				}
 			}
 			break;
 		case 'end':
