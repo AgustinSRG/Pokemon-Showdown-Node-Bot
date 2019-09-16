aa80ab3e53a8e6e27b10a15e27995411fb6d8f39
We Wuz Nidokangz <globalistgamer@gmail.com>
2019-09-16 22:43:15 +0900

    Update Spotlight

--- features/mashups/index.js
+++ features/mashups/index.js
@@ -47,7 +47,7 @@ var tourMetaData = exports.tourMetaData = {};
 
 var completedTourAuthTypeArray = exports.completedTourAuthTypeArray = [];
 
-var spotlightTourNameArray = exports.spotlightTourNameArray = ['[Gen 7] Camomons Ubers'];
+var spotlightTourNameArray = exports.spotlightTourNameArray = ['[Gen 7] Camomons Balanced Hackmons', '[Gen 7] Camomons BH'];
 var spotlightTourNameIdArray = exports.spotlightTourNameIdArray = [];
 var spotlightTourNameGenericIdArray = exports.spotlightTourNameGenericIdArray = [];
 
